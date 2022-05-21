2d9f5335cde87b39227bfaafb8d0e9414c93c26c
Andrei Burdulescu <andrei.burdulescu@pm.me>
2022-05-21 14:28:13 +0300

    start services and wait them

--- main.go
+++ main.go
@@ -3,11 +3,14 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
 	"os"
 	"os/exec"
+	"path/filepath"
 	"strings"
 
 	"bandr.me/baxs/conf"
+	"golang.org/x/sys/unix"
 )
 
 func main() {
@@ -17,41 +20,90 @@ func main() {
 	}
 }
 
-type Service struct {
+type DaemonConf struct {
+	LogsDir string `conf:"logs_dir"`
+}
+
+type ServiceConf struct {
 	Command string `conf:"command"`
 }
 
 func run() error {
 	configPath := flag.String("c", "./baxs.conf", "path to config file")
 	flag.Parse()
+
 	f, err := os.Open(*configPath)
 	if err != nil {
 		return err
 	}
 	defer f.Close()
+
 	var config conf.Conf
 	if err := config.Parse(f); err != nil {
 		return err
 	}
+
+	var daemonConf DaemonConf
+	if s := config.GetSection("daemon"); s == nil {
+		return fmt.Errorf("'daemon' section not found in config file")
+	} else {
+		if err := s.To(&daemonConf); err != nil {
+			return err
+		}
+	}
+
 	sectionFilter := func(name string) bool {
 		return strings.HasPrefix(name, "service:")
 	}
-	var services []Service
+	var services []ServiceConf
 	for _, s := range config.GetSections(sectionFilter) {
-		var svc Service
+		var svc ServiceConf
 		if err := s.To(&svc); err != nil {
 			return err
 		}
 		services = append(services, svc)
 	}
+
+	os.Mkdir(daemonConf.LogsDir, 0755)
+
 	for _, svc := range services {
 		fmt.Println("run:", svc.Command)
 		args := strings.Split(svc.Command, " ")
-		b, err := exec.Command(args[0], args[1:]...).CombinedOutput()
+		outfile, err := os.Create(filepath.Join(daemonConf.LogsDir, args[0]+".out"))
 		if err != nil {
 			return err
 		}
-		fmt.Println(string(b))
+		errfile, err := os.Create(filepath.Join(daemonConf.LogsDir, args[0]+".err"))
+		if err != nil {
+			return err
+		}
+		cmd := exec.Command(args[0], args[1:]...)
+		cmd.Stdout = outfile
+		cmd.Stderr = errfile
+		if err := cmd.Start(); err != nil {
+			return err
+		}
+		log.Printf("%s started with pid %v\n", args[0], cmd.Process.Pid)
 	}
+
+	for range services {
+		var ws unix.WaitStatus
+		var ru unix.Rusage // TODO: use it
+		wpid, err := unix.Wait4(-1, &ws, 0, &ru)
+		if err != nil {
+			return err
+		}
+		switch {
+		case ws.Exited():
+			log.Printf("pid %v exited with exit code %d\n", wpid, ws.ExitStatus())
+		case ws.Signaled():
+			log.Printf("pid %v terminated by signal %d\n", wpid, ws.Signal())
+		case ws.Stopped():
+			log.Printf("pid %v stopped by signal %d\n", wpid, ws.StopSignal())
+		default:
+			log.Printf("pid %v status %d\n", wpid, ws)
+		}
+	}
+
 	return nil
 }
