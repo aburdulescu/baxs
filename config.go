43831efca34e49facc2277e6f4cc73a612e64aeb
Andrei Burdulescu <andrei.burdulescu@pm.me>
2022-12-26 12:55:04 +0200

    Add some doc comments

--- config.go
+++ config.go
@@ -1,15 +1,25 @@
 package main
 
+// Config specifies the configuration parameters needed by the daemon at startup
 type Config struct {
-	Daemon   DaemonConfig
+	// Daemon configuration
+	Daemon DaemonConfig
+
+	// List of services
 	Services []ServiceConfig
 }
 
+// DaemonConfig specifies the configuration parameters of the daemon
 type DaemonConfig struct {
+	// Path where to save the logs of the daemon and the services
 	LogsDir string
 }
 
+// ServiceConfig specifies the configuration parameters of a service
 type ServiceConfig struct {
-	Name    string
+	// Service name, must be unique
+	Name string
+
+	// Command to execute
 	Command string
 }
---
+++ doc.go
@@ -0,0 +1,6 @@
+// Baxs is a background service supervisor.
+//
+// It is composed of a daemon which owns a set of processes and
+// command line application which iteracts with the daemon to control
+// those processes.
+package main
